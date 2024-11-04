44c1c6c7499aae38393a5b73022b714d166672e1
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2024-11-04 20:12:53 +0800

    feat: predict membership tier

--- membership.py
+++ membership.py
@@ -1,4 +1,5 @@
 from tabulate import tabulate
+import math
 
 class Member:
     users_data = {
@@ -40,3 +41,32 @@ class Member:
         headers = [self.info_headers[i] for i in [0,3,4]]
 
         return print(tabulate(requirements, headers))
+    
+    def predict_membership(self, monthly_expense, monthly_income):
+        silver_row = [i for i in self.membership_info if i[0] == 'Silver']
+        silver_expense = silver_row[0][3]
+        silver_income = silver_row[0][4]
+        silver_pred = math.sqrt((monthly_expense - silver_expense)**2 + (monthly_income - silver_income)**2)
+
+        gold_row = [i for i in self.membership_info if i[0] == 'Gold']
+        gold_expense = gold_row[0][3]
+        gold_income = gold_row[0][4]
+        gold_pred = math.sqrt((monthly_expense - gold_expense)**2 + (monthly_income - gold_income)**2)
+
+        platinum_row = [i for i in self.membership_info if i[0] == 'Platinum']
+        platinum_expense = platinum_row[0][3]
+        platinum_income = platinum_row[0][4]
+        platinum_pred = math.sqrt((monthly_expense - platinum_expense)**2 + (monthly_income - platinum_income)**2)
+
+        if(min(silver_pred, gold_pred, platinum_pred) == silver_pred):
+            tier_prediction = 'Silver'
+        elif(min(silver_pred, gold_pred, platinum_pred) == gold_pred):
+            tier_prediction = 'Gold'
+        elif(min(silver_pred, gold_pred, platinum_pred) == platinum_pred):
+            tier_prediction = 'Platinum'
+
+        if(self.tier not in self.membership_tiers):
+            self.tier = tier_prediction
+            return print(f"Hasil perhitungan Euclidean Distance dari user {self.username} adalah (Platinum: {platinum_pred}, Gold: {gold_pred}, Silver: {silver_pred}) \n Kesimpulan Tier: {tier_prediction}")
+        else:
+            return print("Akun anda sudah teregistrasi")
